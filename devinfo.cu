9c48e5cbdabeba29116a992c79bddd2cd06c11a5
Arnav Sheth <arnavms@stanford.edu>
2026-08-06 19:59:17 -0700

    device properties

--- devinfo.cu
+++ devinfo.cu
@@ -10,5 +10,14 @@ int main() {
 	}
 
 	printf("CUDA devices found: %d\n", count);
+	cudaDeviceProp prop;
+	cudaGetDeviceProperties(&prop, 0);
+	printf("Device name: %s\n", prop.name);
+	printf("  Compute capability   : %d.%d\n", prop.major, prop.minor);
+	printf("  SM count             : %d\n", prop.multiProcessorCount);
+	printf("  Global memory        : %.2f GB\n", prop.totalGlobalMem / 1073741824.0);
+	printf("  Shared mem per block : %.1f KB\n", prop.sharedMemPerBlock / 1024.0);
+	printf("  Max threads per block: %d\n", prop.maxThreadsPerBlock);
+	printf("  Warp size            : %d\n", prop.warpSize);
 	return 0;
 }
